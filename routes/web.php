4242a35fbd8c8c8c8c84b57c68ea96fcafb1fab5
vagrant <vagrant@homestead>
2019-06-22 08:11:34 +0000

    集成了支付宝，并用沙箱测试支付成功

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,12 @@ Route::get('products/{product}', 'ProductsController@show')->name('products.show
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
 Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
 Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+
+
+Route::get('alipay', function() {
+    return app('alipay')->web([
+        'out_trade_no' => time(),
+        'total_amount' => '1',
+        'subject' => 'test subject - 测试'
+    ]);
+});
