24ae3eb62dbd2221d4531f407ba9f0521cfb18cb
Jeff <1191881019@qq.com>
2019-06-27 13:45:46 +0800

    composer elasticsearch/elasticsearch

--- config/database.php
+++ config/database.php
@@ -82,6 +82,11 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'elasticsearch' => [
+            // Elasticsearch 支持多台服务器负载均衡，因此这里是一个数组
+            'hosts' => explode(',', env('ES_HOSTS')),
+        ]
+
     ],
 
     /*
